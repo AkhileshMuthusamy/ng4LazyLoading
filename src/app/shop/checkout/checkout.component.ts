656da1b598803371623a4377c46e0b142bc1eb23
Akhilesh <AkhileshM@outlook.in>
2019-08-17 15:02:59 +0530

    Added jsdoc comments

--- src/app/shop/checkout/checkout.component.ts
+++ src/app/shop/checkout/checkout.component.ts
@@ -1,15 +1,26 @@
 import { Component, OnInit } from '@angular/core';
-
+/**
+ * Think this should work
+ */
 @Component({
   selector: 'app-checkout',
   templateUrl: './checkout.component.html',
   styleUrls: ['./checkout.component.css']
 })
 export class CheckoutComponent implements OnInit {
+  /**
+   * Unneccessay constructor
+   */
+  constructor() {}
+  /** Got added during boilerplate code */
+  ngOnInit() {}
 
-  constructor() { }
-
-  ngOnInit() {
+  /**
+   * Add product to cart
+   * @param id Product Id
+   * @param product Proudct Name
+   */
+  addItemToCart(id: number, product: string) {
+    console.log('Item added');
   }
-
 }
