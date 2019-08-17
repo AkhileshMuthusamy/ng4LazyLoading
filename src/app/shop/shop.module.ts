806061973836f426544337f1c77d3cf7c5934dec
Akhilesh <AkhileshM@outlook.in>
2019-08-17 11:36:41 +0530

    Added child routes

--- src/app/shop/shop.module.ts
+++ src/app/shop/shop.module.ts
@@ -2,11 +2,16 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { CheckoutComponent } from './checkout/checkout.component';
 import { Routes, RouterModule } from '@angular/router';
+import { CartComponent } from './cart/cart.component';
+import { PaymentComponent } from './checkout/payment/payment.component';
 
-const routes: Routes = [{ path: '', component: CheckoutComponent }];
+const routes: Routes = [
+  { path: '', component: CheckoutComponent, children: [{ path: 'payment', component: PaymentComponent }] },
+  { path: 'cart', component: CartComponent }
+];
 
 @NgModule({
   imports: [CommonModule, RouterModule.forChild(routes)],
-  declarations: [CheckoutComponent]
+  declarations: [CheckoutComponent, CartComponent, PaymentComponent]
 })
 export class ShopModule {}
